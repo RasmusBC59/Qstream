8afbd0ab53ffb5a36773c97f48569febf204ca2a
Torbjoern <torbjoern.rasmsusen@nbi.ku.dk>
2023-06-30 15:45:55 +0200

    added step and triangle PSB macros, also added live_plotting_examples notebook to show how to setup the live plotting

--- qstream/qm/OPX_live_controller_qua_macros.py
+++ qstream/qm/OPX_live_controller_qua_macros.py
@@ -10,6 +10,7 @@ from qm.qua import (
     wait,
     measure,
     play,
+    ramp,
     save,
     fixed,
     demod,
@@ -46,7 +47,7 @@ def angle_step_macro_maker(
     print(f"{elements[0]} step: {det_min[elements[0]]/dividers[elements[0]]*jump_amp}V")
     print(f"{elements[1]} step: {det_min[elements[1]]/dividers[elements[1]]*jump_amp}V")
 
-    print('these voltages are in OPX output voltages')
+    print("these voltages are in OPX output voltages")
     print(f"{elements[0]} step: {det_min[elements[0]]}V")
     print(f"{elements[1]} step: {det_min[elements[1]]}V")
 
@@ -81,3 +82,51 @@ def angle_step_macro_maker(
         play(jump_pulse * amp(det_min[elements[1]]), elements[1], duration=4)
 
     return angle_step_macro, inverse_angle_step_macro
+
+
+def PSB_tringle(
+    M_point_V,
+    E_point_V,
+    S_point_V,
+    ramp_time_ns,
+    gate_1,
+    gate_2,
+    jump_pulse="CW",
+    jump_pulse_amp=0.25,
+    dividers=np.array([1, 1]),
+):
+    # M_point_V = np.array([0, 0])
+    M_point_V = M_point_V * dividers
+    E_point_V = E_point_V * dividers
+    S_point_V = S_point_V * dividers
+
+    M_to_E_jump = E_point_V - M_point_V
+    E_to_S_jump = S_point_V - E_point_V
+    S_to_M_jump = (M_point_V - S_point_V).tolist()
+
+    M_to_E_ramp_rate = (M_to_E_jump / ramp_time_ns).tolist()
+    E_to_S_ramp_rate = (E_to_S_jump / ramp_time_ns).tolist()
+
+    ramp_time_clk_cycles = ramp_time_ns // 4
+
+    def triangle_macro():
+        play(ramp(M_to_E_ramp_rate[0]), gate_1, ramp_time_clk_cycles)
+        play(ramp(M_to_E_ramp_rate[1]), gate_2, ramp_time_clk_cycles)
+
+        play(ramp(E_to_S_ramp_rate[0]), gate_1, ramp_time_clk_cycles)
+        play(ramp(E_to_S_ramp_rate[1]), gate_2, ramp_time_clk_cycles)
+
+        play(jump_pulse * amp(S_to_M_jump[0] / jump_pulse_amp), gate_1, 4)
+        play(jump_pulse * amp(S_to_M_jump[1] / jump_pulse_amp), gate_2, 4)
+
+    def inverse_triangle_macro():
+        play(ramp(-M_to_E_ramp_rate[0]), gate_1, ramp_time_clk_cycles)
+        play(ramp(-M_to_E_ramp_rate[1]), gate_2, ramp_time_clk_cycles)
+
+        play(ramp(-E_to_S_ramp_rate[0]), gate_1, ramp_time_clk_cycles)
+        play(ramp(-E_to_S_ramp_rate[1]), gate_2, ramp_time_clk_cycles)
+
+        play(jump_pulse * amp(-S_to_M_jump[0] / jump_pulse_amp), gate_1, 4)
+        play(jump_pulse * amp(-S_to_M_jump[1] / jump_pulse_amp), gate_2, 4)
+
+    return triangle_macro, inverse_triangle_macro
